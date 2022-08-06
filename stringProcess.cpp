142ffb4c08fb451bc278c2abd291046c31028006
Zijun Yang <zijunhz@126.com>
2022-08-06 21:08:48 +0800

    add license

--- stringProcess.cpp
+++ stringProcess.cpp
@@ -1,3 +1,20 @@
+// Copyright (C) 2022 Zijun Yang <zijun.yang@outlook.com>
+//
+// This file is part of Markdown to HTML Convertor.
+//
+// Markdown to HTML Convertor is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Markdown to HTML Convertor is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Markdown to HTML Convertor.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "stringProcess.h"
 #include <cctype>
 #include <string>
